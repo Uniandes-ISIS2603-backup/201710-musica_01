1f0f67808a43a0c4e71ec9cf4d802a156be2da82
jc.bustamante143 <jc.bustamante143@ISIS2603-0116.sis.virtual.uniandes.edu.co>
2017-03-14 21:31:05 -0500

    Add Ciudad and Lugar.

--- musica-01-web/src/main/java/co/edu/uniandes/csw/musica/resources/CiudadResource.java
+++ musica-01-web/src/main/java/co/edu/uniandes/csw/musica/resources/CiudadResource.java
@@ -5,6 +5,7 @@
  */
 package co.edu.uniandes.csw.musica.resources;
 
+import co.edu.uniandes.csw.musica.dtos.CiudadDTO;
 import co.edu.uniandes.csw.musica.dtos.CiudadDetailDTO;
 import co.edu.uniandes.csw.musica.ejbs.CiudadLogic;
 import co.edu.uniandes.csw.musica.entities.CiudadEntity;
@@ -12,7 +13,6 @@ import co.edu.uniandes.csw.musica.exceptions.BusinessLogicException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -21,8 +21,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -35,9 +33,6 @@ import javax.ws.rs.core.MediaType;
 public class CiudadResource {
     
     @Inject private CiudadLogic ciudadLogic;
-    @Context private HttpServletResponse response;
-    @QueryParam("page") private Integer page;
-    @QueryParam("limit") private Integer maxRecords;
     
     private List<CiudadDetailDTO> listEntity2DTO(List<CiudadEntity> entityList){
         List<CiudadDetailDTO> list = new ArrayList<>();
@@ -56,30 +51,22 @@ public class CiudadResource {
     
     @GET
     @Path("{id: \\d+}")
-    public CiudadDetailDTO getCiudad(@PathParam("id") Long id) {
-        return new CiudadDetailDTO(ciudadLogic.getCiudad(id));
+    public CiudadDTO getCiudad(@PathParam("id") Long id) {
+        return new CiudadDTO(ciudadLogic.getCiudad(id));
     }
     
     
     @POST
-    public CiudadDetailDTO createCiudad(CiudadDetailDTO dto) throws BusinessLogicException {
-        return new CiudadDetailDTO(ciudadLogic.createCiudad(dto.toEntity()));
+    public CiudadDTO createCiudad(CiudadDTO dto) throws BusinessLogicException {
+        return new CiudadDTO(ciudadLogic.createCiudad(dto.toEntity()));
     }
-    
-    @POST
-    @Path("{id: \\d+}")
-    public CiudadDetailDTO createCiudad(@PathParam("id") Long id, CiudadDetailDTO dto)
-    {
-        return null;
-    }
-    
     
     @PUT
     @Path("{id: \\d+}")
-    public CiudadDetailDTO updateCiudad(@PathParam("id") Long id, CiudadDetailDTO dto) {
+    public CiudadDTO updateCiudad(@PathParam("id") Long id, CiudadDTO dto) {
         CiudadEntity entity = dto.toEntity();
         entity.setId(id);
-        return new CiudadDetailDTO(ciudadLogic.updateCiudad(entity));
+        return new CiudadDTO(ciudadLogic.updateCiudad(entity));
     }
     
     
