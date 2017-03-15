54d850e91123b4c612591fb930ec8fbe397543bf
la.herrera11 <la.herrera11@ISIS2603-0126.sis.virtual.uniandes.edu.co>
2017-03-15 03:24:11 -0500

    Chambios en Boleta y Funcion

--- musica-01-web/src/main/java/co/edu/uniandes/csw/musica/resources/FuncionResource.java
+++ musica-01-web/src/main/java/co/edu/uniandes/csw/musica/resources/FuncionResource.java
@@ -6,14 +6,19 @@
 package co.edu.uniandes.csw.musica.resources;
 
 import co.edu.uniandes.csw.musica.dtos.FuncionDTO;
+import co.edu.uniandes.csw.musica.dtos.MusicoDTO;
 import co.edu.uniandes.csw.musica.ejbs.FuncionLogic;
+import co.edu.uniandes.csw.musica.ejbs.MusicoLogic;
 import co.edu.uniandes.csw.musica.entities.FuncionEntity;
+import co.edu.uniandes.csw.musica.entities.MusicoEntity;
+import co.edu.uniandes.csw.musica.exceptions.BusinessLogicException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -30,7 +35,7 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class FuncionResource {
     @Inject private FuncionLogic funcionLogic;
-    
+    @Inject private MusicoLogic musicoLogic;
     /**
  * Obtiene la lista de funciones
  * 
@@ -57,6 +62,27 @@ public FuncionDTO getFuncion(@PathParam("id") Long id)
 {
 return new FuncionDTO(funcionLogic.getFuncion(id));
 }
+
+
+
+/**
+ * Metodo que dado un id de un musico, lo agrega a la funcion con id dado por parametro
+ * @param id, id de la funcion 
+ * @param idMusico, id del musico que se desea agregar a la funcion
+ * @return MusicoDTO del musico guardado en la funcion id del festival
+ * @throws BusinessLogicException 
+ */
+@POST
+@Path("{id :\\d+}/musicos/{idMusico:\\d+}")
+public MusicoDTO addMusicoFuncion(@PathParam("id") Long id,@PathParam("idMusico") Long idMusico)throws BusinessLogicException{
+   
+    MusicoEntity entity = new MusicoDTO(musicoLogic.getMusico(idMusico)).toEntity();
+    FuncionEntity funcion = new FuncionDTO(funcionLogic.getFuncion(id)).toEntity();
+    funcion.getMusicos().add(entity);
+    funcionLogic.updateFuncion(funcion);
+    return new MusicoDTO(entity); 
+}
+
 
 /**
  * Actualiza la funcion con el id dado por parámetro
