92835a69883f7ea803496028cc9057263ab6acd9
Christian Potdevin <c.potdevin10@uniandes.edu.co>
2017-04-26 09:11:36 -0500

    Add UsuarioEntity documentation and @PodamExclude lines.

--- musica-01-logic/src/main/java/co/edu/uniandes/csw/musica/entities/UsuarioEntity.java
+++ musica-01-logic/src/main/java/co/edu/uniandes/csw/musica/entities/UsuarioEntity.java
@@ -13,87 +13,155 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import uk.co.jemos.podam.common.PodamExclude;
 
+/**
+ * Clase que modela un usuario que puede ser cliente o administrador del
+ * sistema de conciertos.
+ */
 @Entity
-
 public class UsuarioEntity implements Serializable
 {
-
+    /**
+     * Numero del ID del usuario en la base de datos.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @PodamExclude
     private Long id;
+    
+    /**
+     * Nombre del usuario.
+     */
     private String nombre;
+    
+    /**
+     * Clave del usuario.
+     */
     private String clave;
+    
+    /**
+     * Boolean que indica si el usuario es administrador o cliente,
+     * si es true el usuario es administrador.
+     */
     private boolean esAdmin;
-
+    
+    /**
+     * Lista de festivales que maneja el administrador.
+     */
     @ManyToMany
+    @PodamExclude
     private List<FestivalEntity> festivalesUsuario;
 
+    /**
+     * Lista de boletas que pertenecen al usuario.
+     */
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    @PodamExclude
     private List<BoletaEntity> boletas;
 
-    public UsuarioEntity()
-    {
-
+    /**
+     * Método constructor vacío.
+     */
+    public UsuarioEntity() {
     }
 
-    public void setClave(String clave) 
-    {
-            this.clave = clave;
+    /**
+     * Método para definir la clave del usuario.
+     * @param clave Clave del usuario.
+     */
+    public void setClave(String clave) {
+        this.clave = clave;
     }
 
-    public String getClave() 
-    {
-            return clave;
+    /**
+     * Método para conseguir la clave del usuario.
+     * @return Clave del usuario.
+     */
+    public String getClave() {
+        return clave;
     }
 
-    public void setId(Long id) 
-    {
-            this.id = id;
+    /**
+     * Método para definir el id del usuario.
+     * @param id Id del usuario.
+     */
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public Long getId() 
-    {
-            return id;
+    /**
+     * Método para conseguir el id del usuario.
+     * @return Id del usuario.
+     */
+    public Long getId() {
+        return id;
     }
 
-    public void setNombre(String nombre) 
-    {
-            this.nombre = nombre;
+    /**
+     * Método para definir el nombre del usuario.
+     * @param nombre Nombre del usuario.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
-    public String getNombre() 
-    {
-            return nombre;
+    /**
+     * Método para conseguir el nombre del usuario.
+     * @return Nombre del usuario
+     */
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setFestivales(List<FestivalEntity> festivales) 
-    {
-            this.festivalesUsuario = festivales;
+    /**
+     * Método para definir la lista de festivales de los que esta encargado
+     * el administrador.
+     * @param festivales Lista de festivales.
+     */
+    public void setFestivales(List<FestivalEntity> festivales) {
+        this.festivalesUsuario = festivales;
     }
 
-    public List<FestivalEntity> getFestivales() 
-    {
-            return festivalesUsuario;
+    /**
+     * Método para conseguir la lista de festivales de los que esta
+     * encargado el administrador.
+     * @return Lista de festivales.
+     */
+    public List<FestivalEntity> getFestivales() {
+        return festivalesUsuario;
     }
 
-    public List<BoletaEntity> getBoletas() 
-    {
-            return boletas;
+    /**
+     * Método para conseguir la lista de boletas del usuario.
+     * @return Lista de boletas.
+     */
+    public List<BoletaEntity> getBoletas() {
+        return boletas;
     }
-    public void setBoletas(List<BoletaEntity> b) 
-    {
-            this.boletas = b;
+    
+    /**
+     * Método para definir la lista de boletas del usuario.
+     * @param boletas Lista de boletas. 
+     */
+    public void setBoletas(List<BoletaEntity> boletas) {
+        this.boletas = boletas;
     }
 
-    public void setEsAdmin(boolean esAdmin) 
-    {
-            this.esAdmin = esAdmin;
+    /**
+     * Método para definir si el usuario es administrador o no.
+     * @param esAdmin Boolean para determinar si el usuario es administrador
+     * o no.
+     */
+    public void setEsAdmin(boolean esAdmin) {
+        this.esAdmin = esAdmin;
     }
 
-    public boolean getEsAdmin()
-    {
+    /**
+     * Método para determinar si un usuario es administrador o no.
+     * @return True si el usario es administrador y false de lo contrario.
+     */
+    public boolean getEsAdmin() {
         return esAdmin;
     }
-
 }
